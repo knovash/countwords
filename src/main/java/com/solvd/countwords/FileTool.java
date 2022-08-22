788d4e5ce8770b164f311cf9c7e82c0a3c086089
unknown <novash.ki@gmail.com>
2022-08-22 10:41:11 +0300

    write to file

--- src/main/java/com/solvd/countwords/FileTool.java
+++ src/main/java/com/solvd/countwords/FileTool.java
@@ -4,13 +4,18 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class FileTool {
 
     public static String getString(String fileName) throws IOException {
-
         File file = new File(fileName);
         String text = FileUtils.readFileToString(file);
         return text;
+    }
+
+    public static void putList(String fileName, List<String> text) throws IOException {
+        File file = new File(fileName);
+        FileUtils.writeLines(file, text);
     }
 }
